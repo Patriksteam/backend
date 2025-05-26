d9dc08ea06530a90ca6bb686b181468726cea562
Patrik <Patrik.Lichtenberger2008@gmail.com>
2025-05-26 14:18:42 +0200

    Automatischer Commit Mo 26. Mai 14:18:42 CEST 2025

--- backend/src/main/java/com/mycompany/backend/LoginRedirectControlle.java
+++ backend/src/main/java/com/mycompany/backend/LoginRedirectControlle.java
@@ -2,7 +2,6 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-
 package com.mycompany.backend;
 
 /**
@@ -10,5 +9,5 @@ package com.mycompany.backend;
  * @author pat
  */
 public class LoginRedirectControlle {
-
+    
 }
