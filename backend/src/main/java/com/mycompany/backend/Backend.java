92b9e6d40e779fdb0669b26b6011a1d70b6e0c29
Patrik <Patrik.Lichtenberger2008@gmail.com>
2025-05-26 13:57:07 +0200

    Automatischer Commit Mo 26. Mai 13:57:07 CEST 2025

--- backend/src/main/java/com/mycompany/backend/Backend.java
+++ backend/src/main/java/com/mycompany/backend/Backend.java
@@ -15,15 +15,14 @@ public class Backend {
     @Bean
     public CommandLineRunner createAdminUser(UserRepository userRepository, PasswordEncoder passwordEncoder) {
         return args -> {
-            if (userRepository.findByEmail("Patrik").isEmpty()) {
+            if (userRepository.findByEmail("peter@gmail.com").isEmpty()) {
                 User admin = new User();
-                admin.setUsername("Admin");
-                admin.setEmail("admin@example.com");
-                admin.setPassword(passwordEncoder.encode("admin123"));
+                admin.setUsername("Peter");
+                admin.setEmail("peter@gmail.com");
+                admin.setPassword(passwordEncoder.encode("max"));
                 admin.setRoles("ADMIN");
                 // Falls du noch weitere Pflichtfelder hast, hier setzen
                 userRepository.save(admin);
-                System.out.println("Admin user created: admin@example.com / admin123");
             }
         };
     }
