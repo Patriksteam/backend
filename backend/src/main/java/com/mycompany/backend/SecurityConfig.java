60ba03ab2bd949802dca3e104fc4dca0cc44952f
Patrik <Patrik.Lichtenberger2008@gmail.com>
2025-05-26 14:04:39 +0200

    Automatisches Update

--- backend/src/main/java/com/mycompany/backend/SecurityConfig.java
+++ backend/src/main/java/com/mycompany/backend/SecurityConfig.java
@@ -3,9 +3,10 @@ package com.mycompany.backend;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.authorization.AuthorizationDecision;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.access.intercept.RequestAuthorizationContext;
 
 @Configuration
 public class SecurityConfig {
